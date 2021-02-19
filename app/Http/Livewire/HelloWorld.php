601bba28d9437dea53dba997324f34ae8c98fd6b
fathularif <fathulcode@gmail.com>
2021-02-19 12:19:30 +0800

    play around with livewire

--- app/Http/Livewire/HelloWorld.php
+++ app/Http/Livewire/HelloWorld.php
@@ -6,9 +6,17 @@ use Livewire\Component;
 
 class HelloWorld extends Component
 {
+    public $name= '';
+    public $loud= false;
+    public $greeting= ['Yo'];
+
     public function render()
     {
-        $name='Jelly';
-        return view('livewire.hello-world',compact('name'));
+        $hullo ="Pak Yu";
+        return view('livewire.hello-world',compact('hullo'));
+
+        /* Not Using public */
+        //  $name='Jelly';
+        //  return view('livewire.hello-world',compact('name'));
     }
 }
--- resources/views/livewire/hello-world.blade.php
+++ resources/views/livewire/hello-world.blade.php
@@ -1,3 +1,20 @@
 <div>
-    Hello {{ $name }}
+    {{-- <input wire:model.debounce.500ms='name' type="text"> --}}
+    {{-- <input wire:model.lazy='name' type="text"> --}}
+    <input wire:model='name' type="text" placeholder="Type here">
+    <input type="checkbox" wire:model="loud" class="">
+    {{-- <select wire:model='greeting' name="" id="" class=""> --}}
+    <select wire:model="greeting" multiple>
+
+        <option >Yo</option>
+        <option >Goodbye</option>
+        <option >Adios</option>
+    </select>
+
+    {{-- {{ $greeting }} {{ strtoupper($name) }}  --}}
+    This is {{ $hullo }}. {{ implode(', ', $greeting) }} {{ strtoupper($name) }} 
+   
+    @if ($loud)
+        !!
+    @endif
 </div>
